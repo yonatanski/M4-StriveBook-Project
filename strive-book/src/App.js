28b4ad3d54e8f62b557747b2bf95b87188f31c21
yonatanski <phpcodeyg@gmail.com>
2021-12-01 02:42:30 +0100

    LatestRelease Component Done  styling left on card

--- strive-book/src/App.js
+++ strive-book/src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import MyNav from "./Components/MyNav"
 import MyFooter from "./Components/MyFooter"
 import Welcome from "./Components/Welcome"
+import LatestRelease from "./Components/LatestRelease"
 
 function App() {
   return (
     <div>
       <MyNav title="StriveBooks" />
       <Welcome />
+      <LatestRelease />
 
       <MyFooter />
     </div>
---
+++ strive-book/src/Components/LatestRelease.jsx
@@ -0,0 +1,32 @@
+import bookdata from "../data/scifi.json"
+import { Container, Row, Col, Card, Button } from "react-bootstrap"
+
+const LatestRelease = () => {
+  return (
+    <Container>
+      <h1>LatestRelease</h1>
+      <Row>
+        {bookdata.map((book) => (
+          <Col xs={12} sm={6} md={4} lg={3} className="mt-3">
+            <Card>
+              <Card.Img
+                variant="top"
+                src={book.img}
+                className=" img-fluid cardimg object-fit-cover"
+              />
+              <Card.Body>
+                <Card.Title>
+                  <small>{book.title}</small>
+                </Card.Title>
+                <Card.Text> Price {book.price}</Card.Text>
+                <p></p>
+                <Button variant="primary">Buy Now !!</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
+}
+export default LatestRelease
